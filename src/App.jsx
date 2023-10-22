82929df8d86c54a7847f63f8cb472ce07fe60704
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-22 19:50:01 +0700

    Change text state to empty array

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 
 const App = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState(data);
+  const [text, setText] = useState([]);
 
   const handleChange = (e) => {
     setCount(e.target.value);
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <main>
       <section className="section-center">
-        <h4>tired of boring lorem ipsum</h4>
+        <h4>tired of boring lorem ipsum?</h4>
         <form className="lorem-form" onSubmit={handleSubmit}>
           <label htmlFor="count">Paragraphs: </label>
           <input
